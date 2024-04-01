18d4c60f732b2d2bee560c8963cd34c09314ded4
iftekharul islam <iftekharulislam.cse@gmail.com>
2024-04-02 01:21:09 +0600

    Head and theme update

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
-  <link rel="icon" href="{{ asset('favicon.ico') }}" />
+  <link rel="icon" href="{{ asset('glab.png') }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vuexy - Vuejs Admin Dashboard Template</title>
+  <title>GLab Sagl - Ecommerce project</title>
   <link rel="stylesheet" type="text/css" href="{{ asset('loader.css') }}" />
   @vite(['resources/js/main.js'])
 </head>
