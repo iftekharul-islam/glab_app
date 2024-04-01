768357aa2864686e187ce80bed5a626e4fb785f2
iftekharul islam <iftekharulislam.cse@gmail.com>
2024-04-02 01:03:15 +0600

    build post fix remove

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [// Docs: https://github.com/posva/unplugin-vue-router
   // ℹ️ This plugin should be placed before vue plugin
     VueRouter({
